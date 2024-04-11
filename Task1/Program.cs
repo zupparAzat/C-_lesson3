45b9bb7d959722bf7c59594c7f75598903295220
Azat Zuppar <zuppar.azat@gmail.com>
2024-04-11 12:21:01 +0500

    add_lesson3_Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numerator = a * b;
+    int denominator = c + d;
+    double result = numerator / denominator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
