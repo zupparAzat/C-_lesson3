f04ac0a99d46905b1bcca64991d54409052c3e60
Azat Zuppar <zuppar.azat@gmail.com>
2024-04-11 14:13:29 +0500

    add_lesson3_Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintSqueres (int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+
+PrintSqueres(5);
+PrintSqueres(10);
+PrintSqueres(15);
